abcc34761663e9011eca439c92303aedfd0f54e2
Maciej Kucharski <maciek.zir@gmail.com>
2015-10-27 17:10:33 +0100

    [master] get rid of redundant variable

--- meteor-workshop/server/accounts.js
+++ meteor-workshop/server/accounts.js
@@ -1,12 +1,11 @@
-Accounts.onCreateUser(function(options,user) {
-	console.log('creating new user '+user._id);
+Accounts.onCreateUser(function(options, user) {
+	console.log('creating new user ' + user._id);
 	// Add Google account info to profile
-	var profile = _.pick(user.services.google,
+	user.profile = _.pick(user.services.google,
 		"name",
 		"given_name",
 		"picture"
 	);
-	user.profile = profile;
 
 	// Create basic interests doc
 	Interests.insert({
